e75e296e28a6676a9581bf399bbb6db5a809505d
kristjan-komlosi <kristjan.komlosi@gmail.com>
2016-11-24 18:54:40 +0100

    24.11.16
    I have been experiencing invalid parameter errors around getdevice function. correct library initialization confirmed.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <CyUSBCommon.h>
-#include <CyUSBSerial.h>
+#include <libusb.h>
 
-
+CY_VID_PID deviceID{0x04b4, 0x0002}; //a variable containing the id of the chip.
+uint8_t *deviceArray = new uint8_t[1024];
+unsigned char deviceCount;
+CY_DEVICE_INFO *deviceInfo = new CY_DEVICE_INFO[1024];
 //----ACTUAL CODE BELOW
 int verifyArgNum(int num) //verifies the number of arguments. minimum is 2
 {
@@ -16,19 +19,24 @@ int verifyArgNum(int num) //verifies the number of arguments. minimum is 2
   return 1;
 }
 
-int initLib() //initializes the cypress library
-{
-    if (CyLibraryInit() != CY_SUCCESS)  //if the init procedure fails
-    {                                   //the library may be running.
-      if (CyLibraryExit() != CY_SUCCESS) //try to close the possibly running library
-      {
-        return 1;                        //if even that fails, abort.
-      }
-      initLib();                          //if the library closed gracefully
-                                          //try to reinitialize
-    }
-
-}
+// int initLib() //initializes the cypress library
+// {
+//   puts("Initializing library...");
+//     if (CyLibraryInit() != CY_SUCCESS)  //if the init procedure fails
+//     {
+//       puts("The library init failed.");
+//       puts("Restarting...");                                   //the library may be running.
+//       if (CyLibraryExit() != CY_SUCCESS) //try to close the possibly running library
+//       {
+//         return 1;                        //if even that fails, abort.
+//       }
+//       puts("OK");
+//       initLib();                          //if the library closed gracefully
+//                                           //try to reinitialize
+//     }
+//   puts("OK");
+//
+// }
 
 void usage()
 {
@@ -41,13 +49,30 @@ void usage()
 
 int locateDevice()  //locate the device and verify it's the right one
 {
-;
+  int c = CyGetDeviceInfoVidPid(deviceID, deviceArray, deviceInfo, &deviceCount, uint8_t(1024));
+  puts("boop");
+  printf("test %i \n", c);
+  if (c != CY_SUCCESS)
+  {
+
+    puts("Device not found.");
+    printf("test: %i %i err %i \n",*deviceArray,deviceCount,c);
+    return -1;
+  }
+  printf("test: %i %i \n",*deviceArray,deviceCount);
+
+
 }
 
 int main(int argc, char const *argv[]) {
-  if (!(verifyArgNum(argc)))
-  {
-    usage();
-  }
-  initLib();
+  // if (!(verifyArgNum(argc)))
+  // {
+  //   usage();
+  //   return 2;
+  // }
+  // initLib();
+  int c = CyLibraryInit();
+  printf("foo %i\n", c );
+  locateDevice();
+  CyLibraryExit();
 }
